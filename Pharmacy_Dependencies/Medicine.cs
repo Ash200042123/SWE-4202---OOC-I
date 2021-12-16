c84a714c48884e97751fb6cfd0784dda037fb263
Ash200042123 <ashfaq@iut-dhaka.edu>
2021-12-16 15:57:37 +0600

    Added Lab 05

---
+++ Pharmacy_Dependencies/Medicine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Dependencies
+{
+    public class Medicine
+    {
+        public string name, ID;
+        public int quantity, price;
+
+        public void sellMedicine( int n)
+        {
+            quantity = quantity - n;
+        }
+        public string getInfo()
+        {
+            string info = ID + "    " + name + "    " + Convert.ToString(quantity);
+            return info;
+        }
+    }
+}
---
+++ Pharmacy_Dependencies/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Dependencies
+{
+    public class User
+    {
+        public string ID;
+        public int balance;
+
+        public void addBalance(int n, int price)
+        {
+            int increase = n * price;
+            balance = balance + increase;
+        }
+        public string getBalance()
+        {
+            return Convert.ToString(balance);
+        }
+    }
+}
